5d40c2235358b5f951ae3848308a7dfe80f7150b
thehashton <harry.ashton.uk@gmail.com>
2024-05-21 04:01:04 +0100

    Adds some padding top to skills section

--- src/components/Home/Skills.jsx
+++ src/components/Home/Skills.jsx
@@ -1,32 +1,34 @@
-'use client';
-import React, { useEffect } from 'react';
+"use client";
+import React, { useEffect } from "react";
 //= Static Data
-import data from '@/data/info.json';
-import isInView from '@/common/isInView';
+import data from "@/data/info.json";
+import isInView from "@/common/isInView";
 
 function Skills() {
   useEffect(() => {
     isInView({
-      selector: '.progres',
+      selector: ".progres",
       isElements: true,
       callback: (element) => {
         element.style.width = element.dataset.value;
       },
       whenOutOfView: (element) => {
         element.style.width = 0;
-      }
+      },
     });
   }, []);
 
   return (
-    <div className="skills section-padding pt-0" data-scroll-index="1">
+    <div className="skills section-padding pt-40" data-scroll-index="1">
       <div className="sec-head bord-thin-bottom pb-20 mb-80">
         <h4 className="sub-title fz-28">Skills</h4>
       </div>
       <div className="row">
         {data.skills.map((item, index) => (
           <div className="col-md-6" key={item.id}>
-            <div className={`item ${index !== data.skills.length - 1 ? 'mb-30' : ''}`}>
+            <div
+              className={`item ${index !== data.skills.length - 1 ? "mb-30" : ""}`}
+            >
               <div className="d-flex align-items-center mb-30">
                 <div className="mr-30">
                   <div className="img icon-img-40">
@@ -46,7 +48,7 @@ function Skills() {
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
