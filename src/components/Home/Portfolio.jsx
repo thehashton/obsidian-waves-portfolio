498148b85ea138397e459221ddc57d34d21b6a5d
thehashton <harry.ashton.uk@gmail.com>
2024-05-31 13:17:37 +0100

    Hides other portfolio items for now

--- src/components/Home/Portfolio.jsx
+++ src/components/Home/Portfolio.jsx
@@ -61,45 +61,45 @@ function Portfolio() {
               </div>
             </div>
           ))}
-          {rest.map((item) => (
-            <div className="col-lg-4" key={item.id}>
-              <div className="item mt-50">
-                <div className="img">
-                  <Link href="/project">
-                    <img src={item.image} alt="" className="radius-5" />
-                  </Link>
-                </div>
-                <div className="cont mt-30 d-flex align-items-center">
-                  <div>
-                    <span className="tag">{item.tag}</span>
-                    <h6 className="line-height-1">
-                      <Link href="/project">{item.title}</Link>
-                    </h6>
-                  </div>
-                  <div className="ml-auto">
-                    <div className="arrow">
-                      <Link href="/project">
-                        <svg
-                          className="arrow-right"
-                          xmlns="http://www.w3.org/2000/svg"
-                          xmlnsXlink="http://www.w3.org/1999/xlink"
-                          x="0px"
-                          y="0px"
-                          viewBox="0 0 34.2 32.3"
-                          xmlSpace="preserve"
-                          style={{ strokeWidth: 2 }}
-                        >
-                          <line x1="0" y1="16" x2="33" y2="16"></line>
-                          <line x1="17.3" y1="0.7" x2="33.2" y2="16.5"></line>
-                          <line x1="17.3" y1="31.6" x2="33.5" y2="15.4"></line>
-                        </svg>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+          {/*{rest.map((item) => (*/}
+          {/*  <div className="col-lg-4" key={item.id}>*/}
+          {/*    <div className="item mt-50">*/}
+          {/*      <div className="img">*/}
+          {/*        <Link href="/project">*/}
+          {/*          <img src={item.image} alt="" className="radius-5" />*/}
+          {/*        </Link>*/}
+          {/*      </div>*/}
+          {/*      <div className="cont mt-30 d-flex align-items-center">*/}
+          {/*        <div>*/}
+          {/*          <span className="tag">{item.tag}</span>*/}
+          {/*          <h6 className="line-height-1">*/}
+          {/*            <Link href="/project">{item.title}</Link>*/}
+          {/*          </h6>*/}
+          {/*        </div>*/}
+          {/*        <div className="ml-auto">*/}
+          {/*          <div className="arrow">*/}
+          {/*            <Link href="/project">*/}
+          {/*              <svg*/}
+          {/*                className="arrow-right"*/}
+          {/*                xmlns="http://www.w3.org/2000/svg"*/}
+          {/*                xmlnsXlink="http://www.w3.org/1999/xlink"*/}
+          {/*                x="0px"*/}
+          {/*                y="0px"*/}
+          {/*                viewBox="0 0 34.2 32.3"*/}
+          {/*                xmlSpace="preserve"*/}
+          {/*                style={{ strokeWidth: 2 }}*/}
+          {/*              >*/}
+          {/*                <line x1="0" y1="16" x2="33" y2="16"></line>*/}
+          {/*                <line x1="17.3" y1="0.7" x2="33.2" y2="16.5"></line>*/}
+          {/*                <line x1="17.3" y1="31.6" x2="33.5" y2="15.4"></line>*/}
+          {/*              </svg>*/}
+          {/*            </Link>*/}
+          {/*          </div>*/}
+          {/*        </div>*/}
+          {/*      </div>*/}
+          {/*    </div>*/}
+          {/*  </div>*/}
+          {/*))}*/}
         </div>
       </div>
     </section>
