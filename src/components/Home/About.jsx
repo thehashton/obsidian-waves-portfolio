c405f90076d055cee704da2a5c3dd6977214fffa
thehashton <harry.ashton.uk@gmail.com>
2024-06-20 17:50:47 +0100

    Adds typing animation

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,5 +1,6 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Typical from "react-typical";
 import { motion } from "framer-motion";
 import data from "@/data/info.json";
 import DotsPattern from "@/components/DotsPattern/DotsPattern";
@@ -10,35 +11,71 @@ const fadeInVariants = {
   visible: (i) => ({
     opacity: 1,
     transition: {
-      delay: i * 0.5, // Adjust the delay as needed
+      delay: i * 0.5,
       duration: 1,
     },
   }),
 };
 
 function About() {
+  const [showH1, setShowH1] = useState(false);
+  const [startTyping, setStartTyping] = useState(false);
+
+  useEffect(() => {
+    const h1Timer = setTimeout(() => {
+      setShowH1(true);
+    }, 500); // Delay of 2 seconds for H1
+
+    const typingTimer = setTimeout(() => {
+      setStartTyping(true);
+    }, 1000); // Delay of 2.5 seconds for typing animation
+
+    return () => {
+      clearTimeout(h1Timer);
+      clearTimeout(typingTimer);
+    };
+  }, []);
+
+  const typingSteps = [
+    "Hi, I'm Harry Ashton a Front-end Developer who loves making great UI 💻",
+    100, // Adjust this delay to slow down the typing effect
+  ];
+
   return (
     <div className="about pt-40" data-scroll-index="1" id="about">
-      <div className="py-4 hidden lg:block">
-        <DotsPattern />
-      </div>
       <motion.div
-        custom={1}
+        custom={5}
         initial="hidden"
         animate="visible"
         variants={fadeInVariants}
-        className="flex flex-wrap cont"
+        className="py-4 hidden lg:block"
       >
-        <h1 className="text-5xl leading-[60px] md:text-6xl md:leading-[80px]">
-          Hi, I'm <span className="main-color font-bold">Harry Ashton</span> a{" "}
-          <span className="bord">
-            Front-end Developer<i></i>
-          </span>{" "}
-          who loves making great UI 💻
-        </h1>
+        <DotsPattern />
       </motion.div>
+      <div className={"min-h-[300px]"}>
+        {showH1 && (
+          <motion.div
+            custom={1}
+            initial="hidden"
+            animate="visible"
+            variants={fadeInVariants}
+            className="flex flex-wrap cont"
+          >
+            <h1 className="text-5xl leading-[60px] md:text-6xl md:leading-[80px]">
+              {startTyping && (
+                <Typical
+                  steps={typingSteps}
+                  wrapper="span"
+                  loop={1}
+                  className="main-color font-bold"
+                />
+              )}
+            </h1>
+          </motion.div>
+        )}
+      </div>
       <motion.div
-        custom={3}
+        custom={5}
         initial="hidden"
         animate="visible"
         variants={fadeInVariants}
@@ -56,14 +93,14 @@ function About() {
         </div>
       </motion.div>
       <motion.div
-        custom={4}
+        custom={6}
         initial="hidden"
         animate="visible"
         variants={fadeInVariants}
         className="text mt-80"
       >
         <motion.h6
-          custom={5}
+          custom={7}
           initial="hidden"
           animate="visible"
           variants={fadeInVariants}
@@ -72,7 +109,7 @@ function About() {
           About Me.
         </motion.h6>
         <motion.p
-          custom={6}
+          custom={8}
           initial="hidden"
           animate="visible"
           variants={fadeInVariants}
